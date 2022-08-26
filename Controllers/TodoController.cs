5d537f812092f4aa176329039f61a5b5ffa58c6c
umashankar2 <uma_shankar2@dell.com>
2022-08-26 18:43:02 +0100

    Add token

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -29,7 +29,16 @@ namespace TodoApi.Controllers
                 _context.TodoItems.Add(new TodoItem { Name = "Item1" });
                 _context.SaveChanges();
             }
+
         }
+        public override void OnActionExecuting(ActionExecutingContext context)
+            {
+                base.OnActionExecuting(context);
+
+                _client.DefaultRequestHeaders.Accept.Clear();
+                _client.DefaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("Bearer", Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"]);
+            }
 
         // GET: api/Todo
         [HttpGet]
